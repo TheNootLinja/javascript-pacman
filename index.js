5ec45c11e05d8a6a9a044a2a1705b69b3d7a37ff
TheNootLinja <thenootlinja@gmail.com>
2020-12-25 16:14:45 -0500

    Add code to render gameboard visually, along with creating the methods on the GameBoard class that we will need later

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup.js';
+
+//Classes
+import GameBoard from './GameBoard';
+
+// DOM Elements
+const gridGame = document.querySelector('#game');
+const gameScore = document.querySelector('#score');
+const gameStart = document.querySelector('#start-button');
+
+// Game Constants
+const POWER_PILL_TIME = 10000;
+const GLOBAL_SPEED = 80;
+const gameBoard = GameBoard.createGameBoard(gridGame, LEVEL);
+
+// Initial Setup
+let score = 0;
+let time = null;
+let winCon = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {
+
+}
+
+function checkCollision(pacman, ghosts) {
+
+}
+
+function gameLoop(pacman, ghosts) {
+
+}
+
+function startGame() {
+
+}
